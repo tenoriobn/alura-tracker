45cf5f819f6e66247d9093f618c1420e6e8e3c97
Bruno Tenório <tenoriobn@gmail.com>
2024-02-29 18:12:57 -0300

    Finalizando desafio de refatorar temporizador e criar um componente Botao reutilizavel

--- src/components/Temporizador.vue
+++ src/components/Temporizador.vue
@@ -2,25 +2,30 @@
   <div class="is-flex is-align-items-center is-justify-content-space-between">
     <Cronometro :tempoEmSegundos="tempoEmSegundos" />
 
-    <button class="button" @click="iniciar" :disabled="cronometroRodando">
-      <span class="icon"><i class="fas fa-play"></i></span>
-      <span>play</span>
-    </button>
-    <button class="button" @click="finalizar" :disabled="!cronometroRodando">
-      <span class="icon"><i class="fas fa-stop"></i></span>
-      <span>stop</span>
-    </button>
+    <Botao
+      @clicado="iniciar"
+      icone="fas fa-play"
+      texto="play"
+      :desabilitado="cronometroRodando"
+    />
+    <Botao
+      @clicado="finalizar"
+      icone="fas fa-stop"
+      texto="stop"
+      :desabilitado="!cronometroRodando"
+    />
   </div>
 </template>
 
 <script lang="ts">
   import { defineComponent } from 'vue';
   import Cronometro from './Cronometro.vue';
+import Botao from './Botao.vue';
 
   export default defineComponent({
     name: 'TemporizadorComponent',
     emits: ['aoTemporizadorFinalizado'],
-    components: { Cronometro },
+    components: { Cronometro, Botao },
     data() {
         return {
             tempoEmSegundos: 0,
