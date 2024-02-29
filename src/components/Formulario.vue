6e4f69d9c1fc43c920f2ad97b515bb9969d2d473
Bruno Tenório <tenoriobn@gmail.com>
2024-02-29 15:27:50 -0300

    Cronometrando tarefas

--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -12,14 +12,14 @@
       <div class="column">
         <div class="is-flex is-align-items-center is-justify-content-space-between">
           <section>
-            <strong>00:00:00</strong>
+            <strong>{{ tempoDecorrido }}</strong>
           </section>
   
-          <button class="button">
+          <button class="button" @click="iniciar">
             <span class="icon"><i class="fas fa-play"></i></span>
             <span>play</span>
           </button>
-          <button class="button">
+          <button class="button" @click="finalizar">
             <span class="icon"><i class="fas fa-stop"></i></span>
             <span>stop</span>
           </button>
@@ -33,6 +33,27 @@
   import { defineComponent } from 'vue';
 
   export default defineComponent({
-    name: 'FormularioComponent'
+    name: 'FormularioComponent',
+    data() {
+      return {
+        tempoEmSegundos: 0
+      }
+    },
+    computed: {
+      tempoDecorrido () : string {
+        return new Date(this.tempoEmSegundos * 1000).toISOString().substr(11,8)
+      }
+    },
+    methods: {
+      iniciar() {
+        setInterval(() => {
+          this.tempoEmSegundos += 1
+        }, 1000)
+        console.log('iniciando')
+      },
+      finalizar() {
+        console.log('finalizando')
+      }
+    }
   });
 </script>
