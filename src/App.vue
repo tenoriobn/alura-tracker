3501d3be8031fd8fe92eb5e7c187b25a74d06e1b
Bruno Tenório <tenoriobn@gmail.com>
2024-03-01 12:43:58 -0300

    Renderizando condicional

--- src/App.vue
+++ src/App.vue
@@ -11,33 +11,44 @@
         <Tarefa 
           v-for="(tarefa, index) in tarefas" 
           :tarefa="tarefa" 
-          :key="index" />
+          :key="index" 
+        />
+
+        <Box v-if="listaEstaVazia">
+          Você não está muito produtivo hoje :(
+        </Box>
       </div>
     </div>
   </main>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import BarraLateral from './components/BarraLateral.vue';
-import Formulario from './components/Formulario.vue';
-import Tarefa from './components/Tarefa.vue';
-import ITarefa from './interfaces/ITarefa'
+  import { defineComponent } from 'vue';
+  import BarraLateral from './components/BarraLateral.vue';
+  import Formulario from './components/Formulario.vue';
+  import Tarefa from './components/Tarefa.vue';
+  import ITarefa from './interfaces/ITarefa'
+  import Box from './components/Box.vue';
 
-export default defineComponent({
-    name: 'App',
-    components: { BarraLateral, Formulario, Tarefa },
-    data() {
-      return {
-        tarefas: [] as ITarefa[]
+  export default defineComponent({
+      name: 'App',
+      components: { BarraLateral, Formulario, Tarefa, Box },
+      data() {
+        return {
+          tarefas: [] as ITarefa[]
+        }
+      },
+      computed: {
+        listaEstaVazia() : boolean {
+          return this.tarefas.length === 0
+        }
+      },
+      methods: {
+        salvarTarefa(tarefa: ITarefa) {
+          this.tarefas.push(tarefa)
+        }
       }
-    },
-    methods: {
-      salvarTarefa(tarefa: ITarefa) {
-        this.tarefas.push(tarefa)
-      }
-    }
-});
+  });
 </script>
 
 <style scoped>
--- src/components/Tarefa.vue
+++ src/components/Tarefa.vue
@@ -1,24 +1,26 @@
 <template>
-  <div class="box has-text-weight-bold">
+  <Box>
     <div class="columns">
-      <div class="column is-7">{{ tarefa.descricao }}</div>
+      <div class="column is-7">{{ tarefa.descricao || 'Tarefa sem descrição' }}</div>
       <div class="column">
         <Cronometro :tempoEmSegundos="tarefa.duracaoEmSegundos" />
       </div>
     </div>
-  </div>
+  </Box>
 </template>
 
 <script lang="ts">
   import { defineComponent, PropType } from 'vue';
   import Cronometro from './Cronometro.vue';
-import ITarefa from '@/interfaces/ITarefa';
+  import ITarefa from '@/interfaces/ITarefa';
+  import Box from './Box.vue';
 
   export default defineComponent({
     name: 'TarefaComponent',
     components: {
-      Cronometro
-    },
+    Cronometro,
+    Box
+},
     props: { 
       tarefa: { 
         type: Object as  PropType<ITarefa>,
@@ -28,8 +30,3 @@ import ITarefa from '@/interfaces/ITarefa';
   });
 </script>
 
-<style scoped>
-  .box {
-    background: #FAF0CA;
-  }
-</style>
